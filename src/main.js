ba216f50c3af1e3432aa6c6da039ce9c26738f85
Amer Mamoun <amer.mn.96@gmail.com>
2019-12-28 23:10:59 +0100

    css fix
    Signed-off-by: Amer Mamoun <amer.mn.96@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 
 import "bootstrap";
-import "bootstrap/dist/css/bootstrap.main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./scss/main.scss";
 import "normalize.css";
 
